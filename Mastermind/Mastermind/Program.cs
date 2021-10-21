2d85108bcc6c43adf59a7424b866053b9e2a6ce6
Galvaoooo <91313024+Galvaoooo@users.noreply.github.com>
2021-10-21 14:51:00 +0200

    Mastermind Array RIcardo no zip

---
+++ Mastermind/Mastermind/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] combi;
+            ChoixCombi(out combi);
+            Afficher(combi);
+        }
+
+        static void ChoixCombi(out int[] combi)
+        {
+            combi = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Random rnd = new Random();
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+
+        static void Afficher(int[] combi){
+            for (int i = 0; i < 4; i++){
+                Console.Write(combi[i]);
+            }
+        }
+
+
+    }
+}
+ 
